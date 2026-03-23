25c7479bc224ceff1c761555eda3d28d9486fe24
G-Wang12 <gordonwang0608@gmail.com>
2026-03-23 12:55:39 -0400

    added readme

--- README.md
+++ README.md
@@ -2,4 +2,23 @@
 some c++ exercise
 
 ## To run
+run 'g++ main.cpp' to get an executable that does the following:
+take max_value_count and max_point_distance as cli arguments and produce "output.txt" in format "start_point:end_point:value_count"
 
+run the executable with 3 optional flags: --gen, --no-points, --no-ranges.
+
+by default, we will feed the points and ranges from points.txt and ranges.txt to our range_set object before writing the calculated value to output.txt
+
+--gen makes the program randomly generate all points and ranges in points.txt and ranges.txt
+
+--no-points indicates we don't want to add the points from points.txt into our range_set instance
+--no-ranges indicates we don't want to add the rangess from range.txt into range_set instance
+
+By default we add all points from points.txt and all ranges from range.txt into range_set instance
+
+## Assumptions
+- For vector<range_t> get_ranges(), we assumed that we can merge ranges even if they don't overlap as long as the new ranges fit the limits of value_count and point_distance. we implemented it so it greedily merges all ranges until the given limits of value_count and point_distance is reached.
+
+- We assumed max_value_count and max_point_distance will be unsigned 32 bit integers.
+
+- we assume it is possible for us to receive start and end points where start is bigger than end. To solve that edge case, we just swap start and end points when it is being added to range.
--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@
 #include <fstream>
 #include <random>
 #include <cstdint>
-// #include <bits/stdc++.h>
-// #include <boost/algorithm/string.hpp>
+
 using namespace std;
 /*
 NOTE: Agentic or chatbot AI use is not allowed. Google (including AI-generated answers to a google query) and Stack Overflow is.
@@ -56,7 +55,7 @@ struct range_t
 class range_set
 {
     map<uint32_t, pair<uint32_t, uint64_t>> iv;
-    // maybe change to uint64_t
+    // could change to uint64_t
     uint32_t max_value_count;
     uint32_t max_point_distance;
     static bool overlaps_or_adjacent(uint32_t a1, uint32_t b1, uint32_t a2, uint32_t b2)
@@ -235,7 +234,6 @@ static bool parse_range_line(const string &s, range_t &r)
 
 int main(int argc, char **argv)
 {
-    // cout << "Hello, World!";
     cin.tie(nullptr);
 
     if (argc < 3)
@@ -265,8 +263,8 @@ int main(int argc, char **argv)
     // by default, we will feed the points and ranges from points.txt and ranges.txt to our range_set object before writing that value to output.txt
     // we will have 3 flags:
     // --gen for when we want to randomly generate all points and ranges into points.txt and ranges.txt
-    // --no-points to indicate we we don't want to add the points from points.txt into range_set
-    // --no-ranges to indicate we we don't want to add the rangess from range.txt into range_set
+    // --no-points to indicate we don't want to add the points from points.txt into range_set
+    // --no-ranges to indicate we don't want to add the rangess from range.txt into range_set
 
     bool do_gen = false;
     bool use_points = true;
@@ -320,10 +318,6 @@ int main(int argc, char **argv)
                 if (parse_range_line(line, r))
                     rs.add_range(r);
             }
-            // uint64_t x;
-            // while (f >> x) {
-            //   if (x <= numeric_limits<uint32_t>::max()) rs.add_point((uint32_t) x);
-            // }
         }
     }
 
